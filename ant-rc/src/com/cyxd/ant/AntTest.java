0c2cf50bc6fdb22e5881e2b1e5286a65a480ab85
Cyxd <xdjiemao@163.com>
2016-11-25 10:13:27 +0800

    init
    ant-rc demo

---
+++ ant-rc/src/com/cyxd/ant/AntTest.java
@@ -0,0 +1,25 @@
+package com.cyxd.ant;
+
+/**
+ * 
+ * @ClassName: AntTest
+ * @Company: 互联在线
+ * @Description: TODO
+ * @author 技术部-任星东
+ * @date 2016年10月9日 下午12:48:57
+ *
+ */
+public class AntTest {
+
+	/** 
+	 * @Title: main 
+	 * @Description: TODO 
+	 * @param @param args     
+	 * @return void     
+	 * @throws 
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Ant!!");
+	}
+
+}
